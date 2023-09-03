b02a87a3c237ff1fe28fa0f2091aca9670c303ea
Marcus Ziadé <marcus.ziade@ouraring.com>
2023-09-03 21:17:04 +0300

    Initial Commit

---
+++ github-appleos/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  github-appleos
+//
+//  Created by Marcus Ziadé on 3.9.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ github-appleos/github_appleosApp.swift
@@ -0,0 +1,17 @@
+//
+//  github_appleosApp.swift
+//  github-appleos
+//
+//  Created by Marcus Ziadé on 3.9.2023.
+//
+
+import SwiftUI
+
+@main
+struct github_appleosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
